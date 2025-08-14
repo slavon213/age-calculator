2380d4b14be1971d67715397c2aca66e77b38606
slavon213 <slavon213@gmail.com>
2025-08-14 15:32:31 +0300

    change html structure add button for calculate

---
+++ script.js
@@ -0,0 +1,22 @@
+const dayInput = document.getElementById("day");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
+const today = new Date();
+
+
+
+
+function integerInput(value) {
+    value = value.replace(/[^\d]+/g, "");
+    return value;
+}
+
+[dayInput, monthInput].forEach((element) => {
+    element.addEventListener("input", function () {
+        this.value = integerInput(this.value).substring(0,2);
+    });
+});
+
+yearInput.addEventListener("input", function(){
+    this.value = integerInput(this.value).substring(0, 4);
+})
