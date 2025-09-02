3acba654a73019dd553056c5a08d972804ff4dd1
slavon213 <slavon213@gmail.com>
2025-09-02 16:52:35 +0300

    add Container and Form components

---
+++ age-calculator/src/components/Form/Form.jsx
@@ -0,0 +1,11 @@
+import styles from './Form.module.css'
+
+const Form = ({children}) => {
+    return (
+        <form className={styles.form}>
+            {children}
+        </form>
+    )
+}
+
+export default Form;
